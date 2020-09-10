136fef8ba2b4a545dcfec237b14a4823e3610594
Awet Tesfay <whitezscope@gmail.com>
2020-09-10 13:25:15 +0200

    clarify the steps by adding discriptive comments and remove unwanted lines of codes and comments from js file

--- Memory game/index.js
+++ Memory game/index.js
@@ -1,75 +1,53 @@
 
-/*
-// array of the image src to be display
-const srcImg = ["img1.jpg", "img2.jpg"];
-
-let i = 1;
-let checkArray = [];
-// this function executed when clicked
-const displayImage = ()=> {
-    let checker, src1;
-    if (i < 2) {
-        src1 = Math.floor(Math.random()*srcImg.length) -1;
-        checker = src1;
-        i++;
-    } else {
-        if (checker == src1) {
-            // display both
-            images[src1].setAttribute("src", `../share/images/${srcImg[src1]}`)
-            checkArray.push(checker);
-            i--;
-            // remove it from the srcImg
-        } else {
-            displayImage();
-        }
-    }
-
-};
- */
-// selected html elements
+// selected html elements of img
 let images = document.querySelectorAll("img");
 
-// listen to the click event of the card
+/* listen to the click event of specifically clicked card by selecting it with its ID
+And call the flipCard function */
 Array.from(document.querySelectorAll(".flip-card")).forEach(card => {
     card.addEventListener("click", ()=> {
         flipCard(card.id);
     });
 })
 
-let checkArray = [];
-let cardArray = [];
+let checkArray = []; // checkArray is to hold the randomly clicked pair srcImg
+let firstCard; // firstCard will save the first clicked card
+let clickCounter = 1; // create counter to count how many times is clicked
 
-let clickCounter = 1; // create counter to count how much is clicked
-
+// flipCard function to flip both cards if they are the same else turn the first card back
 const flipCard = (imageId)=> {
     // array of the srcImg
     let srcImg = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg"];
+
     // get the card that is clicked at the moment
     let card = document.getElementById(`${imageId}`);
+
     // generate random index to be used for assign a random srcImg
     let randomIndex = Math.floor(Math.random()*srcImg.length);
     let randomSrcImg = srcImg[randomIndex]; // get the randomSrcImg
+    checkArray.push(randomSrcImg); // update the checkArray with the randomSrcImg
 
     // check if it is the first click or the second click
     if (clickCounter < 2) {
-        cardArray.push(card);
-        checkArray.push(randomSrcImg);
+        firstCard = card; // save the first clicked card
+
+        // assign the randomSrcImg to the hidden image
         document.querySelector(`#${card.id} .flip-card-back img`).setAttribute("src", `../share/images/${randomSrcImg}`);
-        card.classList.toggle("check");
-        clickCounter++;
+        card.classList.toggle("check"); // flip the card to display the hidden image
+        clickCounter++; // increment the clickCounter to specify that first click is done
     } else {
-        checkArray.push(randomSrcImg);
-        console.log(checkArray);
+        // in second click check if both the randomSrcImg are same
         if (checkArray[0] == checkArray[1]) {
+            // if they do display both of them
             document.querySelector(`#${card.id} .flip-card-back img`).setAttribute("src", `../share/images/${randomSrcImg}`);
             card.classList.toggle("check");
         } else {
-            // flip the first cards back
-            cardArray[0].classList.toggle("check");
+            // if they dont flip the first cards back
+            firstCard.classList.toggle("check");
         }
-        cardArray = [];
-        clickCounter--;
-        checkArray = [];
+        firstCard = ""; // remove the first card that was clicked
+        clickCounter--; // decrement the clickCounter to it initial value
+        checkArray = []; // remove the randomSrcImg which are already checked
 
     }
 }
@@ -82,19 +60,6 @@ const flipCard = (imageId)=> {
 // if they do display the second card
 // if they don't close both
 
-
-// on click call the displayImage function
-// displayImage function =>
-// 1. get random number
-// 2. take the src at the index of // 1. of the srcImg array
-// 3. save it variable a and display the image
-// then comes the second round
-// 4. get random another number
-// 5. take the src at the index of // 4. of the srcImg array
-// 6. save it variable b
-// 7. check if a and b are the same =>
-// 8. if they do display the second images
-// 9. if not restore the first image to background image
 
 
 
